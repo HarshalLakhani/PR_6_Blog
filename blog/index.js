9ef2c5e60352ee340841744a46fe9db35c439c08
Harshal Lakhani <107047750+HarshalLakhani@users.noreply.github.com>
2023-12-11 12:37:11 +0530

    comit

---
+++ blog/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const connect = require("./config/db");
+const cookie = require("cookie-parser");
+const router = require("./routes/user.route");
+const blog = require("./routes/blog.route");
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cookie())
+app.set("view engine", "ejs")
+app.use(express.static(__dirname + "/public"))
+app.set("views", (__dirname + "/views"))
+
+app.use("/user", router)
+app.use("/blog", blog)
+app.use("/", (req, res) => {
+    res.status(200).send("Welcome to the movie API")
+})
+app.listen(8090, () => {
+    console.log("Server 8090 start");
+    connect()
+})
